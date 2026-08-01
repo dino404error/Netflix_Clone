aed6654e56087e1b85abe834dfee9a8e7f49855c
dino404error <yoeditography50526@gmail.com>
2026-08-02 00:51:44 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
 
 dotenv.config();
 
-connectDB();
+const connectDB = require("./config/db");
 
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("🎬 Netflix Backend Running Successfully!");
-});
 
-app.get("/api/message", (req, res) => {
-    res.json({
-        message: "Frontend Connected Successfully!"
-    });
-});
+app.use("/api/auth", require("./routes/authRoutes.js"));
+app.use("/api/movies", require("./routes/movieRoutes.js"));
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(5000,()=>{
+    console.log("Server running on port 5000");
 });
